bf38ca134c9d6772aab89d61cf25889f0ef4e98b
Kevin Chan <kevinykchan@gmail.com>
2020-07-09 17:49:52 -0400

    CircleCI updated

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -9,7 +9,7 @@ dockerpath=game123/mlapi
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run game123-mlapi --image=game123/mlapi --port=80 --generator=run-pod/v1
+kubectl run game123-mlapi --image=$dockerpath --port=80 --generator=run-pod/v1 --labels app="game123-mlapi"
 
 # Step 3:
 # List kubernetes pods
@@ -17,4 +17,8 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward game123-mlapi-5ccbf9bc8-sxbrb 8000:80
+# kubectl port-forward game123-mlapi-5ccbf9bc8-sxbrb 8000:80
+echo "sleeping for 70 seconds while waiting for pod to come up"
+sleep 70
+kubectl port-forward game123-mlapi 8000:80 
+kubectl logs --selector app=game123-mlapi
